2752fa2363a711b29754c5ab6ed04906872a982a
Matt <mateuservico@gmail.com>
2023-05-15 10:32:08 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppProjeto0404/Controllers/FabricantesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ExercicioProjWeb23.Models;
+
+namespace ExercicioProjWeb23.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(
+                context.Fabricantes.OrderBy(c => c.Nome)
+            );
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Fabricantes/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        // POST: Fabricantes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            context.Fabricantes.Remove(fabricante);
+            context.SaveChanges();
+            TempData["Message"] = "Fabricante \"" + fabricante.Nome + "\" foi removido";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = EntityState.Modified;
+                context.SaveChanges();
+                TempData["Message"] = "Fabricante \"" + fabricante.Nome + "\" foi modificado";
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            TempData["Message"] = "Fabricante \"" + fabricante.Nome + "\" foi registrado";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebAppProjeto0404/Models/Categorico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExercicioProjWeb23.Models
+{
+    public class Categorico
+    {
+        public string Nome { get; set; }
+        public long CategoricoId { get; set; }
+    }
+}
